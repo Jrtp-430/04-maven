6e8c207397f26f7fb8d62448f707c2fbad89e164
Jrtp-430 <ramaraogartam430@gmail.com>
2021-11-17 16:29:51 -0700

    add constractor

--- src/main/java/AshokIt/git/HpPrinter.java
+++ src/main/java/AshokIt/git/HpPrinter.java
@@ -1,9 +1,13 @@
 package AshokIt.git;
 
 public class HpPrinter implements IPrinter {
+	public HpPrinter() {
+		System.out.println("HpPrinter.HpPrinter()");
+	}
 
 	@Override
 	public void print() {
+		System.out.println("print the risepte by using Hp Printer");
 		
 	}
 	public void m1() {
